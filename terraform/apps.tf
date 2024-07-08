920a9e997e009d88a11df8f6a0f41bce35f30726
Jason Johnson <Jason.Johnson@devops.ncia.nato.int>
2024-07-08 09:48:32 +0000

    use different sku for app plan

--- terraform/apps.tf
+++ terraform/apps.tf
@@ -9,7 +9,7 @@ resource "azurerm_service_plan" "main" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   os_type             = "Linux"
-  sku_name            = "Y1"
+  sku_name            = "P1v2"
 }
 
 data "namep_azure_name" "fa" {
