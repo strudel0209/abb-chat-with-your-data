05985b2e1f8dba2763e3d807207e01683f28c0d4
Jason Johnson <Jason.Johnson@devops.ncia.nato.int>
2024-07-10 09:43:22 +0000

    add system assigned ID and some roles

--- terraform/ai.tf
+++ terraform/ai.tf
@@ -55,5 +55,33 @@ resource "azurerm_search_service" "main" {
   location            = azurerm_resource_group.main.location
   sku                 = "standard"
 
+  identity {
+    type = "SystemAssigned"
+  }
+
   local_authentication_enabled = false
+}
+
+resource "azurerm_role_assignment" "si_admin" {
+  scope                = azurerm_search_service.main.id
+  role_definition_name = "Search Index Data Contributor"
+  principal_id         = azurerm_linux_web_app.admin.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "si_fa" {
+  scope                = azurerm_search_service.main.id
+  role_definition_name = "Search Index Data Contributor"
+  principal_id         = azurerm_linux_function_app.main.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "si_webapp" {
+  scope                = azurerm_search_service.main.id
+  role_definition_name = "Search Index Data Contributor"
+  principal_id         = azurerm_linux_web_app.docker.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "si_user" {
+  scope                = azurerm_search_service.main.id
+  role_definition_name = "Search Index Data Contributor"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
