b8e2026f4150fe9859fca0ff9f22e92c73df9280
Jason Johnson <Jason.Johnson@devops.ncia.nato.int>
2024-07-10 09:44:06 +0000

    move storage accounts to own file

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -9,30 +9,6 @@ resource "azurerm_resource_group" "main" {
   location = var.location
 }
 
-resource "random_id" "storage_account" {
-  byte_length = 4
-}
-
-data "namep_azure_name" "sa" {
-  name     = "mvp"
-  location = var.location
-  type     = "azurerm_storage_account"
-
-  extra_tokens = {
-    rnd = lower(random_id.storage_account.hex)
-  
-  }
-}
-
-resource "azurerm_storage_account" "main" {
-  name                            = data.namep_azure_name.sa.result
-  resource_group_name             = azurerm_resource_group.main.name
-  location                        = azurerm_resource_group.main.location
-  account_tier                    = "Standard"
-  account_replication_type        = "LRS"
-  allow_nested_items_to_be_public = false
-}
-
 data "namep_azure_name" "law" {
   name     = "mvp"
   location = var.location
---
+++ terraform/storage_accounts.tf
@@ -0,0 +1,40 @@
+resource "random_id" "storage_account" {
+  byte_length = 4
+}
+
+data "namep_azure_name" "sa" {
+  name     = "mvp"
+  location = var.location
+  type     = "azurerm_storage_account"
+
+  extra_tokens = {
+    rnd = lower(random_id.storage_account.hex)
+  }
+}
+
+resource "azurerm_storage_account" "main" {
+  name                            = data.namep_azure_name.sa.result
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = false
+}
+
+resource "azurerm_storage_container" "documents" {
+  name                  = "documents"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "config" {
+  name                  = "config"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
+
+resource "azurerm_role_assignment" "sa_admin_blob_contrib" {
+  scope                = azurerm_storage_account.main.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_linux_web_app.admin.identity[0].principal_id
+}
