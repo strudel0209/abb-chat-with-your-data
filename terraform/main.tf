a7b766f742a846ec3a685a9b4b88c49ba8ec6975
Jason Johnson <jasojohnson@microsoft.com>
2024-07-30 11:42:46 +0000

    purge cognative accounts on destroy

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,9 @@ provider "azurerm" {
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
+    cognitive_account {
+      purge_soft_delete_on_destroy = true
+    }
     key_vault {
       purge_soft_delete_on_destroy    = true
       recover_soft_deleted_key_vaults = true
